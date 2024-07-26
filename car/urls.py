109d55d262377f8861d7cd88c8895f2e224a9273
ombatii <brianogeto896@gmail.com>
2024-07-26 05:50:50 +0300

    Updated static property

--- car/urls.py
+++ car/urls.py
@@ -20,11 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('carpanel.urls')),
-]
-
-# Only serve static files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+  path('admin/', admin.site.urls),
+  path('', include('carpanel.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
