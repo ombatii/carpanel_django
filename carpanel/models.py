f4966936f99395eaea3728a5d8cda4b1b579ddff
ombatii <brianogeto896@gmail.com>
2024-07-26 03:31:52 +0300

    Initial commit

---
+++ carpanel/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+import datetime
+
+
+# Create your models here.
+# Categories of Products
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+
+# All of our Products
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=12)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=1000, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/product/')
+    # Add Sale Stuff
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=10)
+
+    def __str__(self):
+        return self.name
+
+
+
---
+++ carpanel/views.py
@@ -0,0 +1,132 @@
+from django.shortcuts import render, redirect
+from .models import Product, Category
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+# Custom decorator to allow access only to unauthenticated users
+def unauthenticated_user(view_func):
+    def _wrapped_view(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home')
+        return view_func(request, *args, **kwargs)
+    return _wrapped_view
+
+# Home view - Only accessible to authenticated users
+@login_required(login_url='login')
+def home(request):
+    products = Product.objects.all()
+    return render(request, 'home.html', {'products': products})
+
+# Login view - Accessible to unauthenticated users only
+@unauthenticated_user
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Have Been Logged In!")
+            return redirect('home')
+        else:
+            messages.error(request, "There was an error, please try again...")
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+# Logout view
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out... Thanks for stopping by...")
+    return redirect('login')
+
+# Register view - Accessible to all users
+@unauthenticated_user
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            # log in user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "Username Created ")
+            return redirect('home')
+        else:
+            messages.error(request, "Whoops! There was a problem Registering, please try again")
+            return redirect('register')
+    else:
+        return render(request, 'register.html', {'form': form})
+
+# Product view - Only accessible to authenticated users
+@login_required(login_url='login')
+def product(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, 'product.html', {'product': product})
+
+# Category view - Only accessible to authenticated users
+@login_required(login_url='login')
+def category(request, foo):
+    foo = foo.replace('-', ' ')
+    try:
+        category = Category.objects.get(name=foo)
+        products = Product.objects.filter(category=category)
+        return render(request, 'category.html', {'products': products, 'category': category})
+    except Category.DoesNotExist:
+        messages.error(request, "That Category Doesn't Exist...")
+        return redirect('home')
+
+# Category summary view - Only accessible to authenticated users
+@login_required(login_url='login')
+def category_summary(request):
+    categories = Category.objects.all()
+    return render(request, 'category_summary.html', {"categories": categories})
+
+# Update user view - Only accessible to authenticated users
+@login_required(login_url='login')
+def update_user(request):
+    if request.user.is_authenticated:
+        current_user = User.objects.get(id=request.user.id)
+        user_form = UpdateUserForm(request.POST or None, instance=current_user)
+
+        if user_form.is_valid():
+            user_form.save()
+            login(request, current_user)
+            messages.success(request, "User Has Been Updated!!")
+            return redirect('home')
+        return render(request, "update_user.html", {'user_form': user_form})
+    else:
+        messages.error(request, "You Must Be Logged In To Access That Page!!")
+        return redirect('home')
+
+# Update password view - Only accessible to authenticated users
+@login_required(login_url='login')
+def update_password(request):
+    if request.user.is_authenticated:
+        current_user = request.user
+        if request.method == 'POST':
+            form = ChangePasswordForm(current_user, request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Your Password Has Been Updated...")
+                login(request, current_user)
+                return redirect('update_user')
+            else:
+                for error in list(form.errors.values()):
+                    messages.error(request, error)
+                return redirect('update_password')
+        else:
+            form = ChangePasswordForm(current_user)
+            return render(request, "update_password.html", {'form': form})
+    else:
+        messages.error(request, "You Must Be Logged In To View That Page...")
+        return redirect('home')
